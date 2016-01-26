016101ca6196477defb2702e571defc3bf90ac0c
yuanchi <eeit54@gmail.com>
2016-01-26 18:03:15 +0800

    1.add entity: SalesDetail.java 2.alter.sql add create table: salesdetail

--- AngryCatERP/sql/alter.sql
+++ AngryCatERP/sql/alter.sql
@@ -1,3 +1,32 @@
 -- 2015-07-20
 ALTER TABLE `angrycat`.`member` ADD COLUMN `toVipEndDate` DATETIME NULL COMMENT 'VIP到期日' AFTER `vipDiscountUseDate`;
-ALTER TABLE `angrycat`.`member` CHANGE `postalCode` `postalCode` VARCHAR(100) CHARSET utf8 COLLATE utf8_bin NULL COMMENT '郵遞區號'; 
+ALTER TABLE `angrycat`.`member` CHANGE `postalCode` `postalCode` VARCHAR(100) CHARSET utf8 COLLATE utf8_bin NULL COMMENT '郵遞區號';
+
+-- 2016-01-25
+CREATE TABLE `angrycat`.`salesdetail`(  
+  `id` VARCHAR(100) NOT NULL,
+  `memberId` VARCHAR(100) COMMENT '會員',
+  `salePoint` VARCHAR(50) COMMENT '銷售點',
+  `saleStatus` VARCHAR(100) COMMENT '銷售狀態',
+  `fbName` VARCHAR(500) COMMENT 'FB名稱',
+  `activity` VARCHAR(500) COMMENT '活動',
+  `modelId` VARCHAR(100) COMMENT '型號',
+  `productName` VARCHAR(500) COMMENT '產品名稱',
+  `price` DOUBLE COMMENT '價格',
+  `memberPrice` DOUBLE COMMENT '會員價',
+  `priority` VARCHAR(10) COMMENT '優先順序',
+  `orderDate` DATETIME COMMENT '訂購日期',
+  `otherNote` VARCHAR(500) COMMENT '其他備註',
+  `checkBillStatus` VARCHAR(200) COMMENT '對帳狀態',
+  `idNo` VARCHAR(50) COMMENT '身分證字號',
+  `discountType` VARCHAR(200) COMMENT '折扣類型',
+  `arrivalStatus` VARCHAR(100) COMMENT '到貨狀態',
+  `shippingDate` DATETIME COMMENT '出貨日',
+  `sendMethod` VARCHAR(200) COMMENT '郵寄方式',
+  `note` VARCHAR(500) COMMENT '備註',
+  `payDate` DATETIME COMMENT '付款日期',
+  `contactInfo` VARCHAR(200) COMMENT '郵寄地址電話',
+  `registrant` VARCHAR(100) COMMENT '登單者',
+  PRIMARY KEY (`id`)
+)
+COMMENT='銷售明細';
