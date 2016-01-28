1e2014ed2a62bb1f3a1dd2c931fccf020b703650
yuanchi <eeit54@gmail.com>
2016-01-28 15:59:19 +0800

    1.some private fields changed to protected 2.change list method's parameter

--- AngryCatERP/code/com/angrycat/erp/web/controller/BaseQueryController.java
+++ AngryCatERP/code/com/angrycat/erp/web/controller/BaseQueryController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
@@ -33,17 +34,23 @@ import com.angrycat.erp.security.User;
 import com.angrycat.erp.service.QueryBaseService;
 import com.angrycat.erp.web.WebUtils;
 import com.angrycat.erp.web.component.ConditionConfig;
-
+/**
+ * 
+ * @author JerryLin
+ *
+ * @param <T> 查詢標的，對應SELECT...SQL語法
+ * @param <R> 查詢來源，對應FROM...SQL語法
+ */
 public abstract class BaseQueryController<T, R> implements Serializable {
 	private static final long serialVersionUID = 3114068155803613052L;
 
 	@Autowired
 	@Qualifier("queryBaseService")
-	private QueryBaseService<T, R> queryBaseService; 
+	protected QueryBaseService<T, R> queryBaseService; 
 	
 	@Autowired
 	@Qualifier("queryBaseService")
-	private QueryBaseService<T, R> findTargetService; 
+	protected QueryBaseService<T, R> findTargetService; 
 	/**
 	 * 取得對應資料夾路徑的模組名稱。
 	 * 預設模組名稱是entity的simple name，而且全為小寫字母。
@@ -57,6 +64,8 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 	}
 	/**
 	 * 首字母小寫的駝峰式模組名稱
+	 * 預設是entity的simple name，將第一個字母改成小寫
+	 * 這符合Java變數命名慣例
 	 * @return
 	 */
 	String getLowerCamelCaseModuleName(){
@@ -66,8 +75,15 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 		String moduleName = firstLetter.toLowerCase() + remaining;
 		return moduleName;
 	}
+	/**
+	 * 取得要處理的entity
+	 * @return
+	 */
 	abstract Class<R> getRoot();
-	
+	/**
+	 * 在controller bean建立之後，進行額外的初始化操作。
+	 * 現階段(2016-01-27)最主要的初始化工作，就是設定查詢條件，並且找到對應session的使用者
+	 */
 	@PostConstruct
 	public void init(){
 		queryBaseService.setRootAndInitDefault(getRoot());
@@ -78,14 +94,6 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 			.createFromAlias(getRoot().getName(), "p")
 			.addWhere(putStr("p.id = :pId"))
 			;
-	}
-	
-	QueryBaseService<T, R> getQueryBaseService(){
-		return queryBaseService;
-	}
-	
-	QueryBaseService<T, R> getFindTargetService(){
-		return findTargetService;
 	}
 	
 	/**
@@ -110,7 +118,7 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 	/**
 	 * 如果找到對應的entity，在傳到前端之前可執行額外動作
 	 */
-	void preForwardToView(){}
+	void preForwardToView(T t){}
 	
 	@RequestMapping(value="/view/{id}",
 			method=RequestMethod.GET)
@@ -126,7 +134,7 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 			return urlPrefix + "/list"; // 如果在查詢和導頁之間，資料被刪掉了，就導回查詢
 		}
 		if(target!=null){
-			preForwardToView();
+			preForwardToView(target);
 		}
 		String result = CommonUtil.parseToJson(target);
 		addUrlPrefixAsModuleName(model);
@@ -134,13 +142,20 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 		return urlPrefix + "/view";
 	}
 	
+	/**
+	 * 導頁至特定模組查詢頁
+	 */
 	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public String list(Model model){
+	public String list(HttpServletRequest request, Model model){
 		String urlPrefix = getModule();
 		addUrlPrefixAsModuleName(model);
 		return urlPrefix + "/list";
 	}
 	
+	/**
+	 * 只清除前端UI的查詢條件，無法跟後端seesion狀態一致。
+	 * 比較完整的方式，是先清掉後端查詢條件，再以此狀態與前端同步。
+	 */
 	@RequestMapping(value="/resetConditions", 
 			method=RequestMethod.GET,
 			produces={"application/xml", "application/json"},
@@ -152,13 +167,18 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 		return result;
 	}
 	
+	/**
+	 * 以預設分頁功能查詢全部資料，
+	 * 所以不會回傳所有資料。
+	 */
 	@RequestMapping(value="/queryAll",
 			method={RequestMethod.POST, RequestMethod.GET},
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
-	public @ResponseBody ConditionConfig<T> queryAll(){
+	public @ResponseBody String queryAll(){
 		ConditionConfig<T> cc = queryBaseService.genCondtitionsAfterExecuteQueryPageable();
-		return cc;
+		String result = conditionConfigToJsonStr(cc);
+		return result;
 	}
 	/**
 	 * 分頁條件查詢
@@ -167,9 +187,10 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 			method=RequestMethod.POST,
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
-	public @ResponseBody ConditionConfig<T> queryConditional(@RequestBody ConditionConfig<T> conditionConfig){
+	public @ResponseBody String queryConditional(@RequestBody ConditionConfig<T> conditionConfig){
 		ConditionConfig<T> cc = queryBaseService.executeQueryPageable(conditionConfig);
-		return cc;
+		String result = conditionConfigToJsonStr(cc);
+		return result;
 	}
 	/**
 	 * 查詢下載Excel之前，先把頁面的條件傳到後端，所以要複製查詢條件
@@ -196,7 +217,7 @@ public abstract class BaseQueryController<T, R> implements Serializable {
 	 */
 	@RequestMapping(value="/downloadExcel", method={RequestMethod.POST, RequestMethod.GET})
 	public void downloadExcel(HttpServletResponse response){
-		File tempFile = getExcelExporter().normal(getQueryBaseService());
+		File tempFile = getExcelExporter().normal(queryBaseService);
 		
 		try(FileInputStream fis = new FileInputStream(tempFile);){
 			writeExcelToResponse(response, fis, getDownloadExcelName());
