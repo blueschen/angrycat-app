a632a78d5cccda12009cf56efc9c168a12c6f9dc
yuanchi <eeit54@gmail.com>
2016-03-17 12:22:09 +0800

    新增EmailContact.java做為Email聯絡人清單

---
+++ AngryCatERP/code/com/angrycat/erp/common/EmailContact.java
@@ -0,0 +1,13 @@
+package com.angrycat.erp.common;
+/**
+ * Email聯絡人清單
+ * @author JerryLin
+ *
+ */
+public class EmailContact {
+	public static final String IFLY = "iflywang@ohmbeads.com.tw";
+	public static final String BLUES = "blueschen@ohmbeads.com.tw";
+	public static final String MIKO = "mikofeng@ohmbeads.com.tw";
+	public static final String JOYCE = "joycechang@ohmbeads.com.tw";
+	public static final String JERRY = "jerrylin@ohmbeads.com.tw";
+}
