ecad96f591139ca9550af250704993f0734b9404
yuanchi <eeit54@gmail.com>
2016-05-10 18:14:43 +0800

    BaseTest.java adds testNestedProperty method

--- AngryCatERP/code/com/angrycat/erp/test/BaseTest.java
+++ AngryCatERP/code/com/angrycat/erp/test/BaseTest.java
@@ -3,8 +3,8 @@ package com.angrycat.erp.test;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.Session;
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 import com.angrycat.erp.initialize.config.RootConfig;
+import com.angrycat.erp.model.Member;
+import com.angrycat.erp.model.SalesDetail;
 
 public class BaseTest {
 	
@@ -67,8 +69,26 @@ public class BaseTest {
 		System.out.println(root);
 	}
 	
+	private static void testNestedProperty(){
+		Member m = new Member();
+		m.setName("memberName");
+		SalesDetail s = new SalesDetail();
+		s.setMember(m);
+		s.setFbName("fbName");
+		
+		String propName1 = "member.name";
+		String propName2 = "fbName";
+		try{
+			System.out.println(PropertyUtils.getNestedProperty(s, propName1));
+			System.out.println(PropertyUtils.getNestedProperty(s, propName2));
+		}catch(Throwable e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
 	public static void main(String[]args){
-		testSystemProperty();
+		testNestedProperty();
 	}
 
 	
