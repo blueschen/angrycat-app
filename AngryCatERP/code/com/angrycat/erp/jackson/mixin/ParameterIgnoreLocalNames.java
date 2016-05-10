f917bb384a4e70dff5bd0a79bea1aa5ec6253e71
yuanchi <eeit54@gmail.com>
2016-05-10 18:03:00 +0800

    add ParameterIgnoreLocalNames.java

---
+++ AngryCatERP/code/com/angrycat/erp/jackson/mixin/ParameterIgnoreLocalNames.java
@@ -0,0 +1,9 @@
+package com.angrycat.erp.jackson.mixin;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public interface ParameterIgnoreLocalNames {
+	@JsonIgnore public Map<String, String> getLocaleNames(); 
+}
