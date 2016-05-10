68807643b8e49d0407d06e70304ab02bcffc7fdb
yuanchi <eeit54@gmail.com>
2016-05-10 18:10:54 +0800

    SalesDetailExcelExporter.java adds test case

--- AngryCatERP/code/com/angrycat/erp/excel/SalesDetailExcelExporter.java
+++ AngryCatERP/code/com/angrycat/erp/excel/SalesDetailExcelExporter.java
@@ -1,5 +1,6 @@
 package com.angrycat.erp.excel;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.context.annotation.Scope;
@@ -8,13 +9,27 @@ import org.springframework.stereotype.Service;
 import com.angrycat.erp.format.FormatListFactory;
 import com.angrycat.erp.format.ObjectFormat;
 import com.angrycat.erp.model.SalesDetail;
+import com.angrycat.erp.service.QueryBaseService;
+import com.angrycat.erp.test.BaseTest;
 @Service
 @Scope("prototype")
 public class SalesDetailExcelExporter extends ExcelExporter<SalesDetail> {
-
+	
 	@Override
 	public List<ObjectFormat> getFormats() {
 		return FormatListFactory.ofSalesDetailForExcelExport();
 	}
 
+	private static void testExport(){
+		BaseTest.executeApplicationContext(acac->{
+			QueryBaseService<SalesDetail, SalesDetail> service = acac.getBean("queryBaseService", QueryBaseService.class);
+			service.setRootAndInitDefault(SalesDetail.class);
+			SalesDetailExcelExporter exporter = acac.getBean(SalesDetailExcelExporter.class);
+			File file = exporter.normal(service);
+		});
+	}
+	
+	public static void main(String[]args){
+		testExport();
+	}
 }
