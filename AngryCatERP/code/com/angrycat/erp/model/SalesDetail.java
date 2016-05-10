f3ca8b2e79d6f2a74de7afa3fcccb556500a43ea
yuanchi <eeit54@gmail.com>
2016-05-10 17:52:06 +0800

    SalesDetail.java memberId property changes to member with type Member

--- AngryCatERP/code/com/angrycat/erp/model/SalesDetail.java
+++ AngryCatERP/code/com/angrycat/erp/model/SalesDetail.java
@@ -6,6 +6,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -19,7 +21,7 @@ public class SalesDetail {
 	public static final String SALE_POINT_ESLITE_DUNNAN = "敦南誠品";
 	
 	private String id;
-	private String memberId;
+	private Member member;
 	
 	private String salePoint;
 	private String saleStatus;
@@ -48,7 +50,7 @@ public class SalesDetail {
 	private String rowId;
 	
 	@Id
-	@Column(name="id")
+	@Column(name="id", columnDefinition="ID")
 	@GenericGenerator(name="angrycat_salesdetail_id", strategy="com.angrycat.erp.ds.TimeUID")
 	@GeneratedValue(generator = "angrycat_salesdetail_id")
 	public String getId() {
@@ -57,168 +59,169 @@ public class SalesDetail {
 	public void setId(String id) {
 		this.id = id;
 	}
-	@Column(name="memberId")
-	public String getMemberId() {
-		return memberId;
+	@ManyToOne(targetEntity=Member.class)
+	@JoinColumn(name="memberId", columnDefinition="會員")
+	public Member getMember() {
+		return member;
 	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
+	public void setMember(Member member) {
+		this.member = member;
 	}
-	@Column(name="salePoint")
+	@Column(name="salePoint", columnDefinition="銷售點")
 	public String getSalePoint() {
 		return salePoint;
 	}
 	public void setSalePoint(String salePoint) {
 		this.salePoint = salePoint;
 	}
-	@Column(name="saleStatus")
+	@Column(name="saleStatus", columnDefinition="狀態")
 	public String getSaleStatus() {
 		return saleStatus;
 	}
 	public void setSaleStatus(String saleStatus) {
 		this.saleStatus = saleStatus;
 	}
-	@Column(name="fbName")
+	@Column(name="fbName", columnDefinition="FB名稱/客人姓名")
 	public String getFbName() {
 		return fbName;
 	}
 	public void setFbName(String fbName) {
 		this.fbName = fbName;
 	}
-	@Column(name="activity")
+	@Column(name="activity", columnDefinition="活動")
 	public String getActivity() {
 		return activity;
 	}
 	public void setActivity(String activity) {
 		this.activity = activity;
 	}
-	@Column(name="modelId")
+	@Column(name="modelId", columnDefinition="型號")
 	public String getModelId() {
 		return modelId;
 	}
 	public void setModelId(String modelId) {
 		this.modelId = modelId;
 	}
-	@Column(name="productName")
+	@Column(name="productName", columnDefinition="明細")
 	public String getProductName() {
 		return productName;
 	}
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-	@Column(name="price")
+	@Column(name="price", columnDefinition="定價")
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	@Column(name="memberPrice")
+	@Column(name="memberPrice", columnDefinition="會員價(實收價格)")
 	public double getMemberPrice() {
 		return memberPrice;
 	}
 	public void setMemberPrice(double memberPrice) {
 		this.memberPrice = memberPrice;
 	}
-	@Column(name="priority")
+	@Column(name="priority", columnDefinition="順序")
 	public String getPriority() {
 		return priority;
 	}
 	public void setPriority(String priority) {
 		this.priority = priority;
 	}
-	@Column(name="orderDate")
+	@Column(name="orderDate", columnDefinition="銷售日期")
 	public Date getOrderDate() {
 		return orderDate;
 	}
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
-	@Column(name="otherNote")
+	@Column(name="otherNote", columnDefinition="其他備註")
 	public String getOtherNote() {
 		return otherNote;
 	}
 	public void setOtherNote(String otherNote) {
 		this.otherNote = otherNote;
 	}
-	@Column(name="checkBillStatus")
+	@Column(name="checkBillStatus", columnDefinition="對帳狀態")
 	public String getCheckBillStatus() {
 		return checkBillStatus;
 	}
 	public void setCheckBillStatus(String checkBillStatus) {
 		this.checkBillStatus = checkBillStatus;
 	}
-	@Column(name="mobile")
+	@Column(name="mobile", columnDefinition="手機")
 	public String getMobile() {
 		return mobile;
 	}
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
-	@Column(name="idNo")
+	@Column(name="idNo", columnDefinition="身份證字號")
 	public String getIdNo() {
 		return idNo;
 	}
 	public void setIdNo(String idNo) {
 		this.idNo = idNo;
 	}
-	@Column(name="discountType")
+	@Column(name="discountType", columnDefinition="折扣説明")
 	public String getDiscountType() {
 		return discountType;
 	}
 	public void setDiscountType(String discountType) {
 		this.discountType = discountType;
 	}
-	@Column(name="arrivalStatus")
+	@Column(name="arrivalStatus", columnDefinition="已到貨")
 	public String getArrivalStatus() {
 		return arrivalStatus;
 	}
 	public void setArrivalStatus(String arrivalStatus) {
 		this.arrivalStatus = arrivalStatus;
 	}
-	@Column(name="shippingDate")
+	@Column(name="shippingDate", columnDefinition="出貨日")
 	public Date getShippingDate() {
 		return shippingDate;
 	}
 	public void setShippingDate(Date shippingDate) {
 		this.shippingDate = shippingDate;
 	}
-	@Column(name="sendMethod")
+	@Column(name="sendMethod", columnDefinition="郵寄方式")
 	public String getSendMethod() {
 		return sendMethod;
 	}
 	public void setSendMethod(String sendMethod) {
 		this.sendMethod = sendMethod;
 	}
-	@Column(name="note")
+	@Column(name="note", columnDefinition="備註")
 	public String getNote() {
 		return note;
 	}
 	public void setNote(String note) {
 		this.note = note;
 	}
-	@Column(name="payDate")
+	@Column(name="payDate", columnDefinition="付款日期")
 	public Date getPayDate() {
 		return payDate;
 	}
 	public void setPayDate(Date payDate) {
 		this.payDate = payDate;
 	}
-	@Column(name="contactInfo")
+	@Column(name="contactInfo", columnDefinition="郵寄地址電話")
 	public String getContactInfo() {
 		return contactInfo;
 	}
 	public void setContactInfo(String contactInfo) {
 		this.contactInfo = contactInfo;
 	}
-	@Column(name="registrant")
+	@Column(name="registrant", columnDefinition="登單者")
 	public String getRegistrant() {
 		return registrant;
 	}
 	public void setRegistrant(String registrant) {
 		this.registrant = registrant;
 	}
-	@Column(name="rowId")
+	@Column(name="rowId", columnDefinition="Excel序號")
 	public String getRowId() {
 		return rowId;
 	}
