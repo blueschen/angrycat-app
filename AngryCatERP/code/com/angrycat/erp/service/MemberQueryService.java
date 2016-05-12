90c2630a4db5ebd338e56c62df971b95f34467bf
yuanchi <eeit54@gmail.com>
2016-05-12 16:57:39 +0800

    MemberQueryService changes to extend KendoUiService

--- AngryCatERP/code/com/angrycat/erp/service/MemberQueryService.java
+++ AngryCatERP/code/com/angrycat/erp/service/MemberQueryService.java
@@ -1,38 +1,21 @@
 package com.angrycat.erp.service;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import com.angrycat.erp.component.SessionFactoryWrapper;
-import com.angrycat.erp.condition.ConditionFactory;
+import com.angrycat.erp.common.CommonUtil;
+import com.angrycat.erp.jackson.mixin.MemberIgnoreDetail;
 import com.angrycat.erp.model.Member;
 
 @Service
 @Scope("prototype")
-public class MemberQueryService extends QueryBaseService<Member, Member> {
-
-	/**
-	 * 
-	 */
+public class MemberQueryService extends KendoUiService<Member, Member> {
 	private static final long serialVersionUID = -647818435220341328L;
-
-	@Autowired
-	public MemberQueryService(SessionFactoryWrapper sfw){
-		super(sfw, Member.class);
-	}
 	
-	@PostConstruct
 	@Override
-	public void init(){
-		super.init();
-		String rootAliasWith = DEFAULT_ROOT_ALIAS + ".";
-		addWhere(ConditionFactory.putStr(rootAliasWith+"name=:pName"))
-		.addWhere(ConditionFactory.putInt(rootAliasWith+"gender=:pGender"))
-		.addWhere(ConditionFactory.putSqlDate(rootAliasWith+"birthday=:pBirthday"))
-		;
-		
-	}
+	public String conditionConfigToJsonStr(Object cc){
+		String json = CommonUtil.parseToJson(cc, Member.class, MemberIgnoreDetail.class);
+		return json;
+	}	
+
 }
