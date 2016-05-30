8305b9224618db8d04a3d7d3268b2f945d4594ef
yuanchi <eeit54@gmail.com>
2016-05-30 12:16:27 +0800

    FormatListFactory adds payType and payStatus to SalesDetail, and change their positions

--- AngryCatERP/code/com/angrycat/erp/format/FormatListFactory.java
+++ AngryCatERP/code/com/angrycat/erp/format/FormatListFactory.java
@@ -33,36 +33,39 @@ public class FormatListFactory {
 		FormatList f = new FormatList();
 		f.setDocTitle("rowId");
 		
-		PropertyFormat orderDate = new PropertyFormat("接單日", "orderDate");
+		PropertyFormat orderDate = new PropertyFormat("銷售日", "orderDate");
 		orderDate.setDateFormat(dateFormat);
 		PropertyFormat shippingDate = new PropertyFormat("出貨日", "shippingDate");
 		shippingDate.setDateFormat(dateFormat);
 		PropertyFormat payDate = new PropertyFormat("付款日", "payDate");
 		payDate.setDateFormat(dateFormat);
 		
-		f.add(new PropertyFormat("會員名稱", "member.name"));
+		f.add(new PropertyFormat("狀態", "saleStatus"));
+		f.add(new PropertyFormat("姓名", "fbName"));
+		f.add(new PropertyFormat("會員資料", "member.name"));
 		f.add(new PropertyFormat("銷售點", "salePoint"));
-		f.add(new PropertyFormat("銷售狀態", "saleStatus"));
-		f.add(new PropertyFormat("FB名稱", "fbName"));
-		f.add(new PropertyFormat("活動類型", "activity"));
 		f.add(new PropertyFormat("型號", "modelId"));
-		f.add(new PropertyFormat("產品名稱", "productName"));
+		f.add(new PropertyFormat("明細", "productName"));
 		f.add(new PropertyFormat("定價", "price"));
-		f.add(new PropertyFormat("會員價", "memberPrice"));
-		f.add(new PropertyFormat("優先順序", "priority"));
-		f.add(new PropertyFormat("手機", "mobile"));
+		f.add(new PropertyFormat("實收", "memberPrice"));
+		f.add(new PropertyFormat("折扣別", "discountType"));
 		f.add(orderDate);
-		f.add(new PropertyFormat("其他備註", "otherNote"));
-		f.add(new PropertyFormat("對帳狀態", "checkBillStatus"));
-		f.add(new PropertyFormat("身分證字號", "idNo"));
-		f.add(new PropertyFormat("折扣類型", "discountType"));
-		f.add(new PropertyFormat("已到貨", "arrivalStatus"));
+		f.add(payDate);
+		f.add(new PropertyFormat("付款別", "payType"));
+		f.add(new PropertyFormat("付款狀態", "payStatus"));
 		f.add(shippingDate);
 		f.add(new PropertyFormat("郵寄方式", "sendMethod"));
-		f.add(new PropertyFormat("備註", "note"));
-		f.add(payDate);
-		f.add(new PropertyFormat("郵寄地址電話", "contactInfo"));
 		f.add(new PropertyFormat("登單者", "registrant"));
+		f.add(new PropertyFormat("備註", "note"));
+		f.add(new PropertyFormat("手機", "mobile"));
+		f.add(new PropertyFormat("身分證", "idNo"));
+		
+		f.add(new PropertyFormat("對帳狀態", "checkBillStatus"));
+		f.add(new PropertyFormat("已到貨", "arrivalStatus"));
+		f.add(new PropertyFormat("郵寄地址電話", "contactInfo"));
+		f.add(new PropertyFormat("活動", "activity"));
+		f.add(new PropertyFormat("順序", "priority"));
+		f.add(new PropertyFormat("其他備註", "otherNote"));
 		
 		return f;
 	}
