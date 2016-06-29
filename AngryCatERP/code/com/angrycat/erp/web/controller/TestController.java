e1a1cf81f2f52e47ee0c22752e1e78adbe1d7f3b
yuanchi <eeit54@gmail.com>
2016-06-29 18:07:34 +0800

    TestController.java changes to one by one

--- AngryCatERP/code/com/angrycat/erp/web/controller/TestController.java
+++ AngryCatERP/code/com/angrycat/erp/web/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.angrycat.erp.web.controller;
 
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -33,6 +38,11 @@ import com.angrycat.erp.web.WebUtils;
 @RequestMapping("/test")
 @Scope("session")
 public class TestController {
+	private static final String ANSWERS = "answers";
+	private static final String EXAMS = "exams";
+	private static final String CURRENT_EXAM_IDX = "currentExamIdx";
+	private static final String REPLIES = "replies";
+	
 	@Autowired
 	private RandomExamService randomExamService;
 	@Autowired
@@ -45,28 +55,77 @@ public class TestController {
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
 	public String view(Model model){
-		List<Exam> exams = setExamGroup();
-		model.addAttribute("showExams", CommonUtil.parseToJson(exams));
-		// TODO 顯示考生之前的分數統計
+//		List<Exam> exams = setExamGroup();
+//		model.addAttribute("showExams", CommonUtil.parseToJson(exams));
+		List<ExamStatistics> found = 
+			sfw.executeSession(s->{
+				ExamStatistics result = findTodayStatistics(s);
+				return Arrays.asList(result);
+			});
+		ExamStatistics es = found.get(0);
+		es.setExaminee(null);
+		List<Integer> scores = 
+			sfw.executeSession(s->{
+				List<Integer> results = findTodayScores(s, es.getId());
+				return results;
+			});
+		Map<String, Object> info = new LinkedHashMap<>();
+		info.put("scores", scores);
+		info.put("statistics", es);
+		
+		model.addAttribute("info", CommonUtil.parseToJson(info));
 		return "test/view";
 	}
-	@RequestMapping(value="/retest",
+	@RequestMapping(value="/startTest",
 			method={RequestMethod.POST, RequestMethod.GET},
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
-	public @ResponseBody String retest(){
+	public @ResponseBody String startTest(){
+		HttpSession s= WebUtils.currentSession();
+		s.setAttribute(CURRENT_EXAM_IDX, null);
+		s.setAttribute(REPLIES, null);
+		
 		List<Exam> exams = setExamGroup();
-		String result = CommonUtil.parseToJson(exams);
+		Exam exam = exams.get(0);
+		int examNum = exams.size(); // 題數
+		Map<String, Object> info = new LinkedHashMap<>();
+		info.put("firstExam", exam);
+		info.put("examNum", examNum);
+		String result = CommonUtil.parseToJson(info);
+		return result;
+	}
+	@RequestMapping(value="/correctAfterReply",
+			method={RequestMethod.POST, RequestMethod.GET},
+			produces={"application/xml", "application/json"},
+			headers="Accept=*/*")
+	public @ResponseBody String correctAfterReply(@RequestBody Exam reply){
+		List<Exam> replies = getReplies();
+		replies.add(reply);
+		Exam answer = getAnswer();
+		IntStream.range(0, reply.getItems().size()).boxed()
+			.forEach(i->{
+				answer.getItems().get(i).setSelected(reply.getItems().get(i).isSelected());
+			});
+		String result = CommonUtil.parseToJson(answer);
+		return result;
+	}
+	@RequestMapping(value="/nextExam",
+			method={RequestMethod.POST, RequestMethod.GET},
+			produces={"application/xml", "application/json"},
+			headers="Accept=*/*")
+	public @ResponseBody String nextExam(){
+		Exam nextExam = getNextExam();
+		String result = CommonUtil.parseToJson(nextExam);
 		return result;
 	}
 	@RequestMapping(value="/score",
 			method={RequestMethod.POST, RequestMethod.GET},
 			produces={"application/xml", "application/json"},
 			headers="Accept=*/*")
-	public @ResponseBody String score(@RequestBody List<Exam> exams){
-		List<Exam> originalExams = (List<Exam>)WebUtils.currentSession().getAttribute("originalExams");
+	public @ResponseBody String score(){
+		List<Exam> answerExams = (List<Exam>)WebUtils.currentSession().getAttribute(ANSWERS);
 		Map<String, List<String>> answers = 
-			originalExams
+			answerExams
 				.stream()
 				.collect(Collectors.toMap(
 					Exam::getId, 
@@ -76,8 +135,10 @@ public class TestController {
 						.map(i->i.getDescription())
 						.collect(Collectors.toList())));
 		int total = 100;
-		int partition = total / originalExams.size();
-		int score = exams.stream().mapToInt(e->{
+		int partition = total / answerExams.size();
+		
+		List<Exam> replies = getReplies();
+		int score = replies.stream().mapToInt(e->{
 			List<String> descriptions = answers.get(e.getId()); // 找到正確解答的題項描述
 			e.getItems().stream().forEach(i->{
 				if(descriptions.contains(i.getDescription())){
@@ -99,21 +160,7 @@ public class TestController {
 		ExamStatistics es = null;
 		List<Integer> scores = null;
 		try{
-			Date today = ts.atStartOfToday();
-			User user = WebUtils.getSessionUser();
-			String queryStatistics = "SELECT es FROM " + ExamStatistics.class.getName() + " es WHERE es.examDate = :today AND es.examinee = :user";
-			List<ExamStatistics> results = s.createQuery(queryStatistics).setDate("today", today).setEntity("user", user).list();
-			
-			if(results.isEmpty()){
-				es = new ExamStatistics();
-				es.setExamDate(today);
-				es.setExaminee(user);
-				s.save(es);
-				s.flush();
-				System.out.println("initialized statistics id:"+ es.getId());
-			}else{
-				es = results.get(0);
-			}
+			es = findTodayStatistics(s);
 			
 			ExamScore scoring = new ExamScore();
 			scoring.setScore(score);
@@ -135,7 +182,7 @@ public class TestController {
 			s.update(es);
 			s.flush();
 			
-			scores = s.createQuery("SELECT es.score FROM " + ExamScore.class.getName() + " es WHERE es.statistics.id = :id ORDER BY es.id DESC").setString("id", es.getId()).list();
+			scores = findTodayScores(s, es.getId());
 			s.clear();
 			tx.commit();
 		}catch(Throwable e){
@@ -147,8 +194,6 @@ public class TestController {
 		
 		es.setExaminee(null);
 		Map<String, Object> info = new LinkedHashMap<>();
-		info.put("corrected", exams);
-		info.put("score", score);
 		info.put("scores", scores);
 		info.put("statistics", es);
 		
@@ -180,9 +225,63 @@ public class TestController {
 		return copyExams;
 	}
 	public List<Exam> setExamGroup(){
-		List<Exam> exams = randomExamService.setExamGroup();
-		WebUtils.currentSession().setAttribute("originalExams", exams);
-		List<Exam> copyExams = copy(exams);
-		return copyExams;
-	}	
+		List<Exam> answerExams = randomExamService.setExamGroup();
+		WebUtils.currentSession().setAttribute(ANSWERS, answerExams);
+		List<Exam> exams = copy(answerExams);
+		WebUtils.currentSession().setAttribute(EXAMS, exams);
+		return exams;
+	}
+	private Exam getNextExam(){
+		int idx = getCurrentExamIdx();
+		int nextIdx = ++idx;
+		HttpSession s = WebUtils.currentSession();
+		List<Exam> exams = (List<Exam>)s.getAttribute(EXAMS);
+		Exam nextExam = exams.get(nextIdx);
+		s.setAttribute(CURRENT_EXAM_IDX, nextIdx);
+		return nextExam;
+	}
+	private Exam getAnswer(){
+		int idx = getCurrentExamIdx();
+		List<Exam> originalExams = (List<Exam>)WebUtils.currentSession().getAttribute(ANSWERS);
+		return originalExams.get(idx);
+	}
+	private int getCurrentExamIdx(){
+		HttpSession s = WebUtils.currentSession();
+		if(s.getAttribute(CURRENT_EXAM_IDX)==null){
+			s.setAttribute(CURRENT_EXAM_IDX ,0);
+		}
+		int idx = (int)s.getAttribute(CURRENT_EXAM_IDX);
+		return idx;
+	}
+	private List<Exam> getReplies(){
+		HttpSession s = WebUtils.currentSession();
+		List<Exam> replies = (List<Exam>)s.getAttribute(REPLIES);
+		if(replies==null){
+			replies = new LinkedList<>();
+			s.setAttribute(REPLIES ,replies);
+		}
+		return replies;
+	}
+	private ExamStatistics findTodayStatistics(Session s){
+		Date today = ts.atStartOfToday();
+		User user = WebUtils.getSessionUser();
+		String queryStatistics = "SELECT es FROM " + ExamStatistics.class.getName() + " es WHERE es.examDate = :today AND es.examinee = :user";
+		List<ExamStatistics> results = s.createQuery(queryStatistics).setDate("today", today).setEntity("user", user).list();
+		ExamStatistics es = null;
+		if(results.isEmpty()){
+			es = new ExamStatistics();
+			es.setExamDate(ts.atStartOfToday());
+			es.setExaminee(WebUtils.getSessionUser());
+			s.save(es);
+			s.flush();
+			System.out.println("initialized statistics id:"+ es.getId());
+		}else{
+			es = results.get(0);
+		}
+		return es;
+	}
+	private List<Integer> findTodayScores(Session s, String statisticsId){
+		List<Integer> scores = s.createQuery("SELECT es.score FROM " + ExamScore.class.getName() + " es WHERE es.statistics.id = :id ORDER BY es.id DESC").setString("id", statisticsId).list();
+		return scores;
+	}
 }
