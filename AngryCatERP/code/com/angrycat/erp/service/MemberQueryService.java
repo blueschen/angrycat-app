e78267107ea12a4472b585ef4b2e77404db59f40
yuanchi <eeit54@gmail.com>
2016-07-22 11:34:27 +0800

    MemberQueryService.java removes method conditionConfigToJsonStr

--- AngryCatERP/code/com/angrycat/erp/service/MemberQueryService.java
+++ AngryCatERP/code/com/angrycat/erp/service/MemberQueryService.java
@@ -3,19 +3,10 @@ package com.angrycat.erp.service;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import com.angrycat.erp.common.CommonUtil;
-import com.angrycat.erp.jackson.mixin.MemberIgnoreDetail;
 import com.angrycat.erp.model.Member;
 
 @Service
 @Scope("prototype")
 public class MemberQueryService extends KendoUiService<Member, Member> {
 	private static final long serialVersionUID = -647818435220341328L;
-	
-	@Override
-	public String conditionConfigToJsonStr(Object cc){
-		String json = CommonUtil.parseToJson(cc, Member.class, MemberIgnoreDetail.class);
-		return json;
-	}	
-
 }
