e77e5f66c637d27f647237bb94c3c29451c420cd
yuanchi <eeit54@gmail.com>
2016-12-08 17:35:34 +0800

    ProductStockExcelImporter.java adds method resolveToDB and annotates several methods with @Deprecated.

--- AngryCatERP/code/com/angrycat/erp/excel/ProductStockExcelImporter.java
+++ AngryCatERP/code/com/angrycat/erp/excel/ProductStockExcelImporter.java
@@ -7,35 +7,115 @@ import static org.apache.poi.ss.util.CellReference.convertColStringToIndex;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.hibernate.Session;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.angrycat.erp.component.SessionFactoryWrapper;
+import com.angrycat.erp.component.XSSFProcessor;
 import com.angrycat.erp.model.Product;
 import com.angrycat.erp.test.BaseTest;
 
 @Component
 @Scope("prototype")
 public class ProductStockExcelImporter {
+	private static final String COLUMN_NAME_型號 = "型號";
+	private static final String COLUMN_NAME_英文名字 = "英文名字";
+	private static final String COLUMN_NAME_定價 = "定價";
+	private static final String COLUMN_NAME_總庫存 = "總庫存";
+	private static final String COLUMN_NAME_系列名 = "系列名";
+	private static final String COLUMN_NAME_品名 = "品名";
+	private static final String COLUMN_NAME_專櫃售價 = "專櫃售價";
+		
 	@Autowired
 	private SessionFactoryWrapper sfw;
+	@Autowired
+	private BeanFactory beanFactory;
 	private String src;
+	private boolean mergeDisabled;
+		
 	public void setSrc(String src){
 		this.src = src;
 	}
+	public boolean isMergeDisabled() {
+		return mergeDisabled;
+	}
+	public void setMergeDisabled(boolean mergeDisabled) {
+		this.mergeDisabled = mergeDisabled;
+	}
+	private XSSFProcessor initProcessor(Workbook wb, String sheetName){
+		XSSFProcessor processor = beanFactory.getBean(XSSFProcessor.class, wb, sheetName, "Z");
+		return processor;
+	}
+	private void persistAsProduct(Session s, Workbook wb, String...sheetNames){
+		String queryByModelId = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
+		for(String sheetName : sheetNames){
+			XSSFProcessor processor = initProcessor(wb, sheetName);
+			
+			processor.iteratorBypassHeader();
+			while(processor.hasNext()){
+				processor.next();
+				String modelId = processor.getStrVal(COLUMN_NAME_型號);
+				if(StringUtils.isBlank(modelId)){
+					continue;
+				}
+				int totalStockQty = processor.getIntValu(COLUMN_NAME_總庫存);
+				String nameEng = processor.getStrVal(COLUMN_NAME_英文名字);
+				double price = processor.getDoubleVal(COLUMN_NAME_定價);
+				price = price != 0 ? price : processor.getDoubleVal(COLUMN_NAME_專櫃售價); 
+				String name = processor.getStrVal(COLUMN_NAME_品名);
+				String seriesName = processor.getStrVal(COLUMN_NAME_系列名);
+				
+				Product p = (Product)s.createQuery(queryByModelId).setString("modelId", modelId).uniqueResult();
+				if(p == null){
+					p = new Product();
+					p.setModelId(modelId);
+					p.setNameEng(nameEng);
+					p.setSuggestedRetailPrice(price);
+					
+					p.setSeriesName(seriesName); // collection商品
+					p.setName(name); // 保養品
+				}
+				p.setTotalStockQty(totalStockQty);
+				
+				if(!mergeDisabled){
+					s.saveOrUpdate(p);
+					s.flush();
+				}
+			}
+			if(!mergeDisabled){
+				s.clear();
+			}
+			System.out.println(sheetName + " toalCount: " + processor.getCurrentRowIdx());
+		}
+	}
+	@Transactional
+	public void resolveToDB(){
+		
+		try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(new File(src)));
+			Workbook wb = WorkbookFactory.create(bis);){
+			
+			Session s = sfw.currentSession();
+			persistAsProduct(s, wb, "一般商品", "Collection 商品", "手環手鏈項鏈", "保養品");
+			
+		}catch(Throwable e){
+			throw new RuntimeException(e);
+		}
+	}
+	@Deprecated
 	@Transactional
 	public void execute(){
 		try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(new File(src)));
@@ -46,21 +126,13 @@ public class ProductStockExcelImporter {
 			int G_英文名字 = convertColStringToIndex("G");
 			int G_定價 = convertColStringToIndex("H");
 			int G_總庫存 = convertColStringToIndex("L");
-			int G_未出貨 = convertColStringToIndex("N");
-			int G_辦公室庫存 = convertColStringToIndex("O");
-			int G_專櫃抽屜 = convertColStringToIndex("P");
-			int G_展示櫃 = convertColStringToIndex("Q");
 			System.out.println("一般商品");
 			iterateUpdateStock(
 				general.iterator(),
 				G_型號,
 				G_英文名字,
 				G_定價,
-				G_總庫存,
-				G_未出貨,
-				G_辦公室庫存,
-				G_專櫃抽屜,
-				G_展示櫃);
+				G_總庫存);
 			
 			Sheet collection = wb.getSheet("Collection 商品");
 			int C_型號 = convertColStringToIndex("F");
@@ -68,71 +140,54 @@ public class ProductStockExcelImporter {
 			int C_定價 = convertColStringToIndex("H");
 			int C_系列名 = convertColStringToIndex("I");
 			int C_總庫存 = convertColStringToIndex("M");
-			int C_未出貨 = convertColStringToIndex("P");
-			int C_辦公室庫存 = convertColStringToIndex("Q");
-			int C_專櫃抽屜 = convertColStringToIndex("R");
-			int C_展示櫃 = convertColStringToIndex("S");
 			System.out.println("Collection 商品");
 			iterateUpdateStock(
 				collection.iterator(),
 				C_型號,
 				C_英文名字,
 				C_定價,
-				C_總庫存,
-				C_未出貨,
-				C_辦公室庫存,
-				C_專櫃抽屜,
-				C_展示櫃);
+				C_總庫存);
 			iterateUpdateSerialName(collection.iterator(), C_型號, C_系列名);
 			Sheet necklace = wb.getSheet("手環手鏈項鏈");
 			int N_型號 = convertColStringToIndex("E");
 			int N_英文名字 = convertColStringToIndex("F");
 			int N_定價 = convertColStringToIndex("G");
 			int N_總庫存 = convertColStringToIndex("K");
-			int N_未出貨 = convertColStringToIndex("N");
-			int N_辦公室庫存 = convertColStringToIndex("O");
-			int N_專櫃抽屜 = convertColStringToIndex("P");
-			int N_展示櫃 = convertColStringToIndex("Q");
-
 			System.out.println("手環手鏈項鏈");
 			iterateUpdateStock(
 				necklace.iterator(),
 				N_型號,
 				N_英文名字,
 				N_定價,
-				N_總庫存,
-				N_未出貨,
-				N_辦公室庫存,
-				N_專櫃抽屜,
-				N_展示櫃);			
+				N_總庫存);			
 			
-//			Sheet other = wb.getSheet("其他配件"); // TODO TownTalk庫存是分開管理，並不在同一張庫存表上
-//			int O_型號 = convertColStringToIndex("E");
-//			int O_商品名稱 = convertColStringToIndex("F");
-//			int O_社團價 = convertColStringToIndex("G");
-//			int O_專櫃價 = convertColStringToIndex("H");
-//			int O_總庫存 = convertColStringToIndex("I");
-//			int O_未出貨 = convertColStringToIndex("K");
-//			int O_辦公室庫存 = convertColStringToIndex("L");
-//			int O_專櫃抽屜 = convertColStringToIndex("M");
-//			int O_中和庫存 = convertColStringToIndex("N");
+			Sheet other = wb.getSheet("保養品");
+			int CARE_型號 = convertColStringToIndex("G");
+			int CARE_品名 = convertColStringToIndex("H");
+			int CARE_專櫃售價 = convertColStringToIndex("J");
+			int CARE_總庫存 = convertColStringToIndex("K");
+			System.out.println("保養品");
+			iterateUpdateCareStock(
+				other.iterator(),
+				CARE_型號,
+				CARE_品名,
+				CARE_專櫃售價,
+				CARE_總庫存);
 		}catch(Throwable e){
 			throw new RuntimeException(e);
 		}
 	}
+	@Deprecated
 	private void iterateUpdateStock(
 		Iterator<Row> iterator, 
 		int 型號, 
 		int 英文名字, 
 		int 定價,
-		int 總庫存,
-		int 未出貨,
-		int 辦公室庫存,
-		int 專櫃抽屜,
-		int 展示櫃){
+		int 總庫存){
 		Session s = sfw.currentSession();
 		String queryByModelId = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
 		int count = 0;
+		List<String> added = new ArrayList<>();
 		while(iterator.hasNext()){
 			Row row = iterator.next();
 			count++;
@@ -143,7 +198,7 @@ public class ProductStockExcelImporter {
 			if(StringUtils.isBlank(modelId)){
 				continue;
 			}
-			System.out.println("第"+count+"筆:"+modelId);
+//			System.out.println("第"+count+"筆:"+modelId);
 			List<Product> products = s.createQuery(queryByModelId).setString("modelId", modelId).list();
 			Product p = null;
 			if(!products.isEmpty()){
@@ -153,23 +208,24 @@ public class ProductStockExcelImporter {
 				p.setModelId(modelId);
 				p.setNameEng(parseCellStrVal(row, 英文名字));
 				p.setSuggestedRetailPrice(parseDouble(row, 定價));
-				System.out.println("新增一筆:"+modelId);
+				added.add(modelId);
 			}
-			int totalStockQty = parseInt(row, 總庫存);
-			int officeStockQty = parseInt(row, 辦公室庫存);
-			int drawerStockQty = parseInt(row, 專櫃抽屜);
-			int showcaseStockQty = parseInt(row, 展示櫃);
-			int notShipStockQty = parseInt(row, 未出貨);
-			
+			int totalStockQty = parseInt(row, 總庫存);			
 			p.setTotalStockQty(totalStockQty);
 			
 //			System.out.println(ReflectionToStringBuilder.toString(p, ToStringStyle.MULTI_LINE_STYLE));
-			
-//			s.saveOrUpdate(p);
-//			s.flush();
+			if(!mergeDisabled){
+				s.saveOrUpdate(p);
+				s.flush();
+			}
 		}
-//		s.clear();
+		if(!mergeDisabled){
+			s.clear();
+		}
+		System.out.println("共處理" + (count-1) + "筆");
+		System.out.println("共新增" + added.size() + "筆:" + added.stream().collect(Collectors.joining(",")));
 	}
+	@Deprecated
 	private void iterateUpdateSerialName(Iterator<Row> iterator, int 型號, int 系列名){
 		Session s = sfw.currentSession();
 		String queryByModelId = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
@@ -191,14 +247,67 @@ public class ProductStockExcelImporter {
 				if(StringUtils.isBlank(p.getSeriesName())){
 					p.setSeriesName(serialName);
 				}
-				System.out.println(modelId+"系列名:"+serialName);
+//				System.out.println(modelId+"系列名:"+serialName);
 			}
-			
-//			s.saveOrUpdate(p);
-//			s.flush();
+			if(!mergeDisabled){
+				s.saveOrUpdate(p);
+				s.flush();
+			}
 		}
-//		s.clear();
+		if(!mergeDisabled){
+			s.clear();
+		}
+//		System.out.println("共處理" + (count-1) + "筆");
 	}
+	@Deprecated
+	private void iterateUpdateCareStock(
+			Iterator<Row> iterator, 
+			int 型號, 
+			int 品名, 
+			int 專櫃售價,
+			int 總庫存){
+			Session s = sfw.currentSession();
+			String queryByModelId = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
+			int count = 0;
+			List<String> added = new ArrayList<>();
+			while(iterator.hasNext()){
+				Row row = iterator.next();
+				count++;
+				if(count == 1){// 標題略過
+					continue;
+				}
+				String modelId = parseCellStrVal(row, 型號);
+				if(StringUtils.isBlank(modelId)){
+					continue;
+				}
+//				System.out.println("第"+count+"筆:"+modelId);
+				List<Product> products = s.createQuery(queryByModelId).setString("modelId", modelId).list();
+				Product p = null;
+				if(!products.isEmpty()){
+					p = products.get(0);
+				}else{
+					p = new Product();
+					p.setModelId(modelId);
+					p.setName(parseCellStrVal(row, 品名));
+					p.setSuggestedRetailPrice(parseDouble(row, 專櫃售價));
+					added.add(modelId);
+				}
+				int totalStockQty = parseInt(row, 總庫存);			
+				p.setTotalStockQty(totalStockQty);
+				
+//				System.out.println(ReflectionToStringBuilder.toString(p, ToStringStyle.MULTI_LINE_STYLE));
+				if(!mergeDisabled){
+					s.saveOrUpdate(p);
+					s.flush();
+				}
+			}
+			if(!mergeDisabled){
+				s.clear();
+			}
+			System.out.println("共處理" + (count-1) + "筆");
+			System.out.println("共新增" + added.size() + "筆:" + added.stream().collect(Collectors.joining(",")));
+		}
+	@Deprecated
 	private static double parseDouble(Row row ,int colIdx){
 		Double val = parseCellNumericVal(row, colIdx);
 		if(val != null){
@@ -206,6 +315,7 @@ public class ProductStockExcelImporter {
 		}
 		return 0;
 	}
+	@Deprecated
 	private static int parseInt(Row row, int colIdx){
 		Double val = parseCellNumericVal(row, colIdx);
 		if(val != null){
@@ -217,10 +327,11 @@ public class ProductStockExcelImporter {
 	private static void testExecute(){
 		BaseTest.executeApplicationContext(acac->{
 			ProductStockExcelImporter pse = acac.getBean(ProductStockExcelImporter.class);
-			String path = "C:\\Users\\JerryLin\\Desktop\\臺灣OHM商品總庫存清單_2016_07_25.xlsx";
+			String path = "C:\\Users\\JerryLin\\Desktop\\臺灣OHM商品總庫存清單_2016_12_07.xlsx";
 			if(new File(path).exists()){
 				System.out.println("path file exists");
-				// TODO 如果要啟用儲存，要取消iterateUpdateStock和iterateUpdateSerialName兩個地方的註解
+				// TODO 如果要啟用儲存，要取消iterateUpdateStock，iterateUpdateSerialName和iterateUpdateCareStock三個地方的註解
+				pse.setMergeDisabled(true);
 				pse.setSrc(path);
 				pse.execute();
 			}
