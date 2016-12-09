fd92cb4447a27fd4c9af1659da7b1a737ca4f2b8
yuanchi <eeit54@gmail.com>
2016-12-09 14:41:32 +0800

    SalesDetailKendoUiService.java updates methods including deleteByIds, batchSaveOrMerge, and findProducts

--- AngryCatERP/code/com/angrycat/erp/service/SalesDetailKendoUiService.java
+++ AngryCatERP/code/com/angrycat/erp/service/SalesDetailKendoUiService.java
@@ -65,15 +65,24 @@ public class SalesDetailKendoUiService extends
 	public List<?> deleteByIds(List<String> ids){
 		List<?> results = super.deleteByIds(ids);
 		List<SalesDetail> targets = (List<SalesDetail>)results;
-		List<Product> products = findProducts(targets, sfw.currentSession());
+		Session s = sfw.currentSession();
+		List<Product> products = findProducts(targets, s);
+		s.clear(); // 之後如果再度查詢，應該要拿資料庫而非記憶體當中的，所以清掉記憶體佔存的products；這個動作可以確保之後異動記錄抓到新舊資料差異。
+		List<Product> productsUpdated = new ArrayList<>();
 		IntStream.range(0, products.size())
 			.boxed()
 			.forEachOrdered(i->{
 				SalesDetail sd = targets.get(i);
 				Product p = products.get(i);
-				updateStock(ACTION_DELETE, sd, p, null);
+				if(null == p){
+					return;
+				}
+				int stockChanged = updateStock(ACTION_DELETE, sd, p, null);
+				if(stockChanged != 0){
+					productsUpdated.add(p);
+				}
 			});
-		productService.batchSaveOrMerge(products, null);
+		productService.batchSaveOrMerge(productsUpdated, null);
 		return results;
 	}
 	@Override
@@ -89,15 +98,18 @@ public class SalesDetailKendoUiService extends
 				s.evict(d);// 轉換的時候，順帶脫離session
 				return d;
 			}).collect(Collectors.toMap(d->d.getId(), Function.identity()));
-		
-		List<Product> products = findProducts(targets, sfw.currentSession());
+		List<Product> products = findProducts(targets, s);
 		// 銷售明細先儲存，是為了得到id
 		List<SalesDetail> details = super.batchSaveOrMerge(targets, before);
+		List<Product> productsUpdated = new ArrayList<>();
 		IntStream.range(0, products.size())
 			.boxed()
 			.forEachOrdered(i->{
-				SalesDetail sd = targets.get(i);
 				Product p = products.get(i);
+				if(null == p){
+					return;
+				}
+				SalesDetail sd = targets.get(i);
 				SalesDetail oldDetail = oldDetailMap.get(sd.getId());
 				String action = null;
 				String oldSaleStatus = null;
@@ -107,35 +119,56 @@ public class SalesDetailKendoUiService extends
 					action = ACTION_UPDATE;
 					oldSaleStatus = oldDetail.getSaleStatus();
 				}
-				updateStock(action, sd, p, oldSaleStatus);
-			});	
-		productService.batchSaveOrMerge(products, null);
+				int stockChanged = updateStock(action, sd, p, oldSaleStatus);
+				if(stockChanged != 0){// 不一定每個銷售明細對應的商品庫存都有異動，有異動才更新
+					productsUpdated.add(p);// TODO 要考量針對同一商品連續異動庫存的狀況
+				}
+			});
+		
+		productService.batchSaveOrMerge(productsUpdated, null);
 		return details;
 	}
+	/**
+	 * 以型號查詢與銷售明細順序一致的商品<br>
+	 * 銷售明細有可能不會對應到商品<br>
+	 * 或者其提供的型號沒有查到商品<br>
+	 * 在上述兩種情況下<br>
+	 * 會以null值呈現
+	 * @param targets
+	 * @param s
+	 * @return
+	 */
 	public List<Product> findProducts(List<SalesDetail> targets, Session s){
-		List<String> modelIds =
-			targets.stream().map(t->t.getModelId()).collect(Collectors.toList());
-//		Map<Integer, SalesDetail> targetAsMap = 
-//			IntStream.range(0, targets.size())
-//				.boxed()
-//				.collect(Collectors.toMap(
-//					i->i, 
-//					i->targets.get(i),
-//					mergeSalesDetailFunction,
-//					mapSalesDetailSupplier)); // 用LinkedHashMap確保順序一致
-//		Map<Integer, String> targetModelIds = 
-//			targetAsMap.entrySet().stream()
-//				.collect(Collectors.toMap(
-//					p->p.getKey(), 
-//					p->p.getValue().getModelId(),
-//					mergeModelIdFunction,
-//					mapModelIdSupplier)); // 用LinkedHashMap確保順序一致
-					
-		String queryProduct = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId IN (:modelIds)";
-		List<Product> products = s.createQuery(queryProduct).setParameterList("modelIds", modelIds).list();
+		String queryProduct = "SELECT p FROM " + Product.class.getName() + " p WHERE p.modelId = :modelId";
+		// 此處要確保記憶體中銷售明細和產品順序的一致性，所以用HQL只能一個一個查
+		List<Product> products = 
+			targets
+				.stream()
+				.map(sd->{
+					String modelId = sd.getModelId();
+					if(StringUtils.isBlank(modelId)){// 沒有提供型號，視作無效商品
+						return null;
+					}
+					// 查到一筆或者沒有結果
+					return (Product)s.createQuery(queryProduct).setString("modelId", modelId).uniqueResult();
+					})
+				.collect(Collectors.toList());
 		return products;		
 	}
-	public Product updateStock(String action, SalesDetail sd, Product p, String oldStatus){
+	/**
+	 * 如果商品需要更動庫存<br>
+	 * 改變庫存數並註記<br>
+	 * 之後回傳更動庫存數<br>
+	 * 1代表庫存加1<br>
+	 * -1代表庫存扣1<br>
+	 * 0代表庫存不變
+	 * @param action
+	 * @param sd
+	 * @param p
+	 * @param oldStatus
+	 * @return
+	 */
+	public int updateStock(String action, SalesDetail sd, Product p, String oldStatus){
 		int stock = 0;
 		String newStatus = sd.getSaleStatus();
 		String saleId = sd.getId();
@@ -155,7 +188,7 @@ public class SalesDetailKendoUiService extends
 		if(stock != 0){
 			p.setTotalStockChangeNote("產生自:"+StringUtils.join(template, "_"));
 		}
-		return p;
+		return stock;
 	}
 	int getStockChanged(String action, String oldStatus, String newStatus){
 		int stockChanged = 0;
