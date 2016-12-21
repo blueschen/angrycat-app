0a81eb061e19d85e2c73363d361863c9daf77880
yuanchi <eeit54@gmail.com>
2016-12-21 15:44:37 +0800

    RegularExpressionTests.java adds one testcase

--- AngryCatERP/src/test/java/com/angrycat/erp/test/RegularExpressionTests.java
+++ AngryCatERP/src/test/java/com/angrycat/erp/test/RegularExpressionTests.java
@@ -34,4 +34,26 @@ public class RegularExpressionTests {
 		assertEquals("p.name ", t1.replaceAll(pattern, ""));
 		assertEquals("p.hint ", t2.replaceAll(pattern, ""));
 	}
+	@Test
+	public void findBrace(){
+		String t1 = "這是好天氣{{modelId}}我想想看";
+		String t2 = "這是好天氣";
+		Pattern p = Pattern.compile("\\{\\{(\\w+)\\}\\}");
+		Matcher m1 = p.matcher(t1);
+		while(m1.find()){
+			int start = m1.start(1);
+			int end = m1.end(1);
+			String v = t1.substring(start, end);
+			System.out.println(v);
+		}
+		Matcher m2 = p.matcher(t2);
+		while(m2.find()){
+			int start = m2.start(1);
+			int end = m2.end(1);
+			String v = t2.substring(start, end);
+			System.out.println(v);
+		}
+		String t3 = "p.names[0].field";
+		System.out.println(t3.substring(0, t3.indexOf("]")+1));
+	}
 }
