3524ec48612d93a0d7625966d6ac457ac9615f86
yuanchi <eeit54@gmail.com>
2016-12-21 15:41:33 +0800

    DetailPropertyFormat.java adds one method changeNameIfHinted and changes the logic of getPropertyObject

--- AngryCatERP/code/com/angrycat/erp/format/DetailPropertyFormat.java
+++ AngryCatERP/code/com/angrycat/erp/format/DetailPropertyFormat.java
@@ -2,36 +2,45 @@ package com.angrycat.erp.format;
 
 import java.util.Collection;
 
-import org.apache.commons.beanutils.PropertyUtils;
+import static com.angrycat.erp.common.CommonUtil.getPropertyVal;
 
 public class DetailPropertyFormat extends PropertyFormat {
-
 	public DetailPropertyFormat(String name, String property) {
 		super(name, property);
 	}
 	@Override
 	public Object getPropertyObject(Object obj){
-		try{
-			String property = getProperty();
-			String masterExpression = getMasterExpression(property);
-			Object master = PropertyUtils.getProperty(obj, masterExpression);
-			if(master==null || !(master instanceof Collection)){
-				return null;
-			}
-			Collection<?> c = Collection.class.cast(master);
-			int size = c.size();
-			int index = getDetailPropertyIndex(property);
-			if(index >= size){
-				return null;
-			}
-			return PropertyUtils.getProperty(obj, property);
-		}catch(Throwable e){
-			throw new RuntimeException(e);
+		String property = getProperty();
+		String masterExpression = getMasterExpression(property);
+		Object master = getPropertyVal(obj, masterExpression);
+		if(master==null || !(master instanceof Collection)){
+			return null;
+		}
+		Collection<?> c = Collection.class.cast(master);
+		int size = c.size();
+		int index = getDetailPropertyIndex(property);
+		if(index >= size){
+			return null;
+		}
+		changeNameIfHinted(obj);
+		return getPropertyVal(obj, property);
+	}
+	private void changeNameIfHinted(Object obj){
+		String name = getName();
+		String field = ComplexDetailPropertyFormat.findNameTemplateField(name);
+		if(field != null){
+			String detailExpression = getDetailExpression(getProperty());
+			Object fieldVal = getPropertyVal(obj, detailExpression + "." + field);
+			setName(name.replace("{{"+field+"}}", fieldVal.toString()));
 		}
 	}
 	static String getMasterExpression(String property){
 		String master = property.substring(0, property.indexOf("["));
 		return master;
+	}
+	static String getDetailExpression(String property){
+		String detail = property.substring(0, property.indexOf("]")+1);
+		return detail;
 	}
 	static int getDetailPropertyIndex(String property){
 		String index = property.substring(property.indexOf("[")+1, property.indexOf("]"));
