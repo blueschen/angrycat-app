60d742c1e25ff56bcf664a373bc343d155889e31
yuanchi <eeit54@gmail.com>
2017-01-03 13:41:03 +0800

    ProductKendoUiServiceTests.java adds two testcases

--- AngryCatERP/src/test/java/com/angrycat/erp/service/ProductKendoUiServiceTests.java
+++ AngryCatERP/src/test/java/com/angrycat/erp/service/ProductKendoUiServiceTests.java
@@ -1,5 +1,8 @@
 package com.angrycat.erp.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.angrycat.erp.initialize.config.RootConfig;
+import com.angrycat.erp.model.Product;
 import com.angrycat.erp.service.ProductKendoUiService.ProductStockReport;
 import com.angrycat.erp.service.magento.MagentoBaseService;
 
@@ -37,5 +41,32 @@ public class ProductKendoUiServiceTests {
 	@Test
 	public void sendHTMLToAdmin(){
 		productKendoUiService.sendHTMLToAdmin("ProductKendoUiService.sendHTMLToAdmin", "<span style='color:red;'>Happy New Year</span>");
+	}
+	@Test
+	public void updateStockIfMagentoIsMore(){
+		productKendoUiService.getMagentoProductService().setBaseUrl(MagentoBaseService.LOCALHOST_BASE_URL);
+		productKendoUiService.getMagentoProductService().setDebug(true);
+		productKendoUiService.updateStockIfMagentoIsMore();
+	}
+	@Test
+	public void asyncUpdateMagentoStock(){
+		Product p1 = new Product();
+		p1.setModelId("TT033");
+		p1.setTotalStockQty(10);
+		
+		Product p2 = new Product();
+		p2.setModelId("galaxy001");
+		p2.setTotalStockQty(15);
+		
+		Product p3 = new Product();
+		p3.setModelId("apple001");
+		p3.setTotalStockQty(23);
+		
+		List<Product> products = Arrays.asList(p1, p2, p3);
+		
+		productKendoUiService.getMagentoProductService().setBaseUrl(MagentoBaseService.LOCALHOST_BASE_URL);
+		productKendoUiService.getMagentoProductService().setDebug(true);
+//		productKendoUiService.asyncUpdateMagentoStock(products); // test not achievable because of asynchronous
+		productKendoUiService.getMagentoProductService().updateStockIfDifferentFromMagento(products);
 	}
 }
