f6c9190d93074cfd2631cc432b02ef15ecc19b1d
yuanchi <eeit54@gmail.com>
2017-03-03 14:59:49 +0800

    add SerialGenerator.java

---
+++ AngryCatERP/code/com/angrycat/erp/genserial/SerialGenerator.java
@@ -0,0 +1,25 @@
+package com.angrycat.erp.genserial;
+
+import java.io.Serializable;
+import java.text.NumberFormat;
+
+public abstract class SerialGenerator<PK extends Serializable & Comparable<PK>, S> {
+	public abstract PK getId();
+	public abstract String getNext();
+	public abstract String getNext(S s);
+	
+	protected String incrementNo(String no){
+		long num = Long.parseLong(no) + 1l;
+		int len = no.length();
+		return formatNo(num, len);
+	}
+	protected String formatNo(long num, int len){
+		NumberFormat nf = NumberFormat.getIntegerInstance();
+		nf.setMaximumIntegerDigits(len);
+		nf.setMinimumIntegerDigits(len);
+		nf.setGroupingUsed(false);
+		String no = nf.format(num);
+		return no;
+	}
+	
+}
