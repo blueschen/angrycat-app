3d3510b842cb37c1768525428e58f8129f168fb9
yuanchi <eeit54@gmail.com>
2017-03-09 17:11:11 +0800

    add AmericanGroupBuyTest.java

---
+++ AngryCatERP/src/test/java/com/angrycat/erp/model/AmericanGroupBuyTest.java
@@ -0,0 +1,69 @@
+package com.angrycat.erp.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import com.angrycat.erp.BaseTestCase;
+import com.angrycat.erp.service.TimeService;
+
+public class AmericanGroupBuyTest extends BaseTestCase {
+	public static final String NO201703 = "AGB201703";
+	
+	public static AmericanGroupBuy new201703Instance(){
+		AmericanGroupBuy agb = new AmericanGroupBuy();
+		agb.setActivity("2017-03美國團");
+		agb.setNo("AGB201703");
+		agb.setDeadline(Date.valueOf(LocalDate.of(2017, 3, 7)));
+		agb.setSubAmtUSDThresholdForGift(new BigDecimal("125"));
+		agb.setGiftValAmtUSD(new BigDecimal("65"));
+		agb.setMultiplier(new BigDecimal("1.1"));
+		agb.setRate(new BigDecimal("33"));
+		agb.setDiscountUSD(new BigDecimal("50"));
+		agb.setDiscountUSDThreshold(new BigDecimal("150"));
+		agb.setServiceChargeNTD(new BigDecimal("800"));
+		return agb;
+	}
+	@Test
+	public void insert(){
+		executor.executeTransaction(s->{
+			String no = NO201703;
+			AmericanGroupBuy agb = (AmericanGroupBuy)s.createQuery("FROM " + AmericanGroupBuy.class.getName() + " a WHERE a.no = :no").setString("no", no).uniqueResult();
+			if(agb == null){
+				agb = new201703Instance();
+				s.save(agb);
+				s.flush();
+			}
+		});
+	}
+	@Test
+	public void insertSalesNoGenerator(){
+		executor.executeTransaction(s->{
+			DefaultSerial ds = (DefaultSerial)s.get(DefaultSerial.class, AmericanGroupBuyOrderForm.SALESNO_GENERATOR_ID);
+			if(ds == null){
+				ds = new DefaultSerial();
+				ds.setId(AmericanGroupBuyOrderForm.SALESNO_GENERATOR_ID);
+				ds.setNo("000000000");
+				ds.setResetNoTo(0);
+				s.save(ds);
+				s.flush();
+			}
+		});
+	}
+	@Test
+	public void isOrderFormDisabled(){
+		AmericanGroupBuy agb = new AmericanGroupBuy();
+		System.out.println(agb.isOrderFormDisabled());
+		
+		agb.setDeadline(new TimeService().atStartOfToday());
+		System.out.println(agb.isOrderFormDisabled());
+		
+		agb.setDeadline(Date.valueOf(LocalDate.of(2017, 3, 8)));
+		System.out.println(agb.isOrderFormDisabled());
+		
+		agb.setDeadline(Date.valueOf(LocalDate.of(2017, 3, 10)));
+		System.out.println(agb.isOrderFormDisabled());
+	}
+}
