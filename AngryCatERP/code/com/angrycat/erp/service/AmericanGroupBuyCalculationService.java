ff283b4c255d448bc040dc7b6426f55b30151d0f
yuanchi <eeit54@gmail.com>
2017-03-14 17:59:09 +0800

    AmericanGroupBuyCalculationService.java modifies calculation logic

--- AngryCatERP/code/com/angrycat/erp/service/AmericanGroupBuyCalculationService.java
+++ AngryCatERP/code/com/angrycat/erp/service/AmericanGroupBuyCalculationService.java
@@ -2,7 +2,6 @@ package com.angrycat.erp.service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Scope;
@@ -34,27 +33,16 @@ public class AmericanGroupBuyCalculationService {
 
 	public AmericanGroupBuyCalculation calculate(){
 		int maxScale = orders.stream().map(r->r.getProductAmtUSD().scale()).max(Integer::compareTo).get();
-		BigDecimal giftMadeUpDiff = BigDecimal.ZERO; // 贈品價差要補的金額
-		// 一批訂單頂多有一筆是贈品
-		Optional<BigDecimal> foundDiff = orders.stream().filter(r->"贈品".equals(r.getSalesType())).map(r->r.getProductAmtUSD()).findFirst();
-		if(foundDiff.isPresent()){
-			giftMadeUpDiff = foundDiff.get();
-		}
-		// 小計不包含備取
+		// 小計=正取總額+贈品補差額(不包含備取)
 		BigDecimal subAmtUSD = orders.stream().filter(r->StringUtils.isNotBlank(r.getSalesType()) && !r.getSalesType().contains("備取")).map(r->r.getProductAmtUSD()).reduce(BigDecimal.ZERO, BigDecimal::add).setScale(maxScale, BigDecimal.ROUND_CEILING);
-		BigDecimal discountUSDThreshold = americanGroupBuy.getDiscountUSDThreshold();
-		// 如果小計包含贈品差價的部分，要先扣除再行比較
-		BigDecimal discountUSD = subAmtUSD.subtract(giftMadeUpDiff).compareTo(discountUSDThreshold) >= 0 ? americanGroupBuy.getDiscountUSD() : BigDecimal.ZERO;
 		BigDecimal multiplier = americanGroupBuy.getMultiplier();
 		BigDecimal rate = americanGroupBuy.getRate();
 		BigDecimal serviceChargeNTD = americanGroupBuy.getServiceChargeNTD();
 		
-		BigDecimal totalAmtNTD = subAmtUSD.subtract(discountUSD).multiply(multiplier).multiply(rate).add(serviceChargeNTD).setScale(0, BigDecimal.ROUND_CEILING);
+		BigDecimal totalAmtNTD = subAmtUSD.multiply(multiplier).multiply(rate).add(serviceChargeNTD).setScale(0, BigDecimal.ROUND_CEILING);
 		
 		AmericanGroupBuyCalculation c = new AmericanGroupBuyCalculation();
 		c.subAmtUSD           = subAmtUSD;
-		c.discountUSDThreshold= discountUSDThreshold;
-		c.discountUSD         = discountUSD;
 		c.multiplier          = multiplier;
 		c.rate                = rate;
 		c.serviceChargeNTD    = serviceChargeNTD;
@@ -68,8 +56,6 @@ public class AmericanGroupBuyCalculationService {
 	public AmericanGroupBuyCalculation calculateScaleToLastNonZero(){
 		AmericanGroupBuyCalculation c = calculate();
 		c.subAmtUSD           = scaleToLastNonZero(c.subAmtUSD);
-		c.discountUSDThreshold= scaleToLastNonZero(c.discountUSDThreshold);
-		c.discountUSD         = scaleToLastNonZero(c.discountUSD);
 		c.multiplier          = scaleToLastNonZero(c.multiplier);
 		c.rate                = scaleToLastNonZero(c.rate);
 		c.serviceChargeNTD    = scaleToLastNonZero(c.serviceChargeNTD);
@@ -101,20 +87,12 @@ public class AmericanGroupBuyCalculationService {
 	}
 	public static class AmericanGroupBuyCalculation {
 		BigDecimal subAmtUSD;
-		BigDecimal discountUSDThreshold;
-		BigDecimal discountUSD;
 		BigDecimal multiplier;
 		BigDecimal rate;
 		BigDecimal serviceChargeNTD;
 		BigDecimal totalAmtNTD;
 		public BigDecimal getSubAmtUSD() {
 			return subAmtUSD;
-		}
-		public BigDecimal getDiscountUSDThreshold() {
-			return discountUSDThreshold;
-		}
-		public BigDecimal getDiscountUSD() {
-			return discountUSD;
 		}
 		public BigDecimal getMultiplier() {
 			return multiplier;
