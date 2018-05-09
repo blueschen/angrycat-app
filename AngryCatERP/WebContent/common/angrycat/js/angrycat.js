4bf3e4111fd876f7077f19208c9f65ca319d29e2
yuanchi <eeit54@gmail.com>
2018-05-09 16:22:45 +0800

    add components

--- AngryCatERP/WebContent/common/angrycat/js/angrycat.js
+++ AngryCatERP/WebContent/common/angrycat/js/angrycat.js
@@ -38,9 +38,41 @@
 		}
 		results.appendChild(li);
 	}
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var decodedCookie = decodeURIComponent(document.cookie);
+	    var ca = decodedCookie.split(';');
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) == ' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length, c.length);
+	        }
+	    }
+	    return "";
+	}	
+	function setCookie(cname, cvalue, exdays) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+ d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+	function reloadIfVerionNotMatched(pageName, version) {
+		var cName = pageName + "_deploy_version",
+			v = getCookie(cName);
+		if(version !== v){
+			setCookie(cName, version, 365);
+			window.location.reload(true);
+		}
+	}
 	angrycat.core = {
 		once: once,
 		minusTimezoneOffset: minusTimezoneOffset,
-		plusTimezoneOffset: plusTimezoneOffset
+		plusTimezoneOffset: plusTimezoneOffset,
+		getCookie: getCookie,
+		setCookie: setCookie,
+		reloadIfVerionNotMatched: reloadIfVerionNotMatched
 	};
 })(window);
