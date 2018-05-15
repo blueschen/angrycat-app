d1e3bfac9b19e1894cbef6d9292c9b4a6ca7a585
yuanchi <eeit54@gmail.com>
2018-05-15 16:58:50 +0800

    ShippingDetailsProcessor.java adds validations and warnings

--- AngryCatERP/code/com/angrycat/erp/excel/ShippingDetailsProcessor.java
+++ AngryCatERP/code/com/angrycat/erp/excel/ShippingDetailsProcessor.java
@@ -4,13 +4,21 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
@@ -35,7 +43,77 @@ public class ShippingDetailsProcessor {
 	    return widthUnits; 
 	} 
 	
-	public static byte[] renderXlsx(byte[] data, Map<String, Object> options) throws Exception{
+	private static Map<String, String> bannedSymbols = new HashMap<>();
+	static{
+		bannedSymbols.put(":", "冒號");
+		bannedSymbols.put("*", "星號");
+		bannedSymbols.put("/", "斜線");
+		bannedSymbols.put("\\", "反斜線");
+		bannedSymbols.put("[", "起始中括號");
+		bannedSymbols.put("]", "結束中括號");
+	}
+	private static final Pattern bannedFormat1 = Pattern.compile("^[a-zA-Z]+([0-9]+)$");
+	
+	private static final ZoneId zoneId = ZoneId.systemDefault();
+	
+	static String validateBannedFormat1(String input){
+		// 根據org.apache.poi.ss.formula.SheetNameFormatter#nameLooksLikePlainCellReference的檢核規則先行判斷
+		// 避免執行到後面在Integer.parseInt的時候因超出範圍丟出例外
+		Matcher m = bannedFormat1.matcher(input);
+		String n = null;
+		while(m.find()){
+			n = m.group(1);
+		}
+		String warning = null;
+		if(n != null && n.length() > 10){
+			warning = input + "字母後方數字超過十位數";
+		}
+		return warning;
+	}
+		
+	static boolean validateCustomer(String customerName, int count, List<String> warnings){
+		Set<String> validates = new LinkedHashSet<>();
+		for(Map.Entry<String, String> banned : bannedSymbols.entrySet()){
+			String symbol = banned.getKey();
+			if(customerName.contains(symbol)){
+				validates.add(banned.getValue() + "("+ symbol +")");
+			}
+		}
+		
+		String prefix = "第" + count + "列客戶";
+		if(!validates.isEmpty()){
+			String msg = prefix +"出現禁用符號：" + validates.stream().collect(Collectors.joining("、"));
+			warnings.add(msg);
+			return false;
+		}
+		String bannedFormat1Msg = validateBannedFormat1(customerName);
+		if(bannedFormat1Msg != null){
+			String msg = prefix + bannedFormat1Msg;
+			warnings.add(msg);
+			return false;
+		}
+		return true;
+	}
+	
+	static boolean validateCustomerCell(Cell firstCell, int count, List<String> warnings){
+		if(firstCell == null){
+			//warnings.add(prefix + "不存在");
+			return false;
+		}
+		int cellType = firstCell.getCellType();
+		if(cellType != Cell.CELL_TYPE_STRING){
+			//warnings.add(prefix + "不是文字");
+			return false;
+		}
+		String customerName = firstCell.getStringCellValue();
+		if(customerName == null || "".equals(customerName.trim())){
+			//warnings.add(prefix + "沒有資料");
+			return false;
+		}
+		return true;
+	}
+	
+	public static byte[] renderXlsx(byte[] data, Map<String, Object> options, List<String> warnings) throws Exception{
 		
 		Map<String, List<String>> shippingDetails = new LinkedHashMap<>();
 		byte[] outputData = null;
@@ -44,30 +122,30 @@ public class ShippingDetailsProcessor {
 			Sheet sheet = workbook.getSheet("內容");
 			
 			if(sheet == null){
+				warnings.add("名稱為「內容」的sheet沒找到");
 				return outputData;
 			}
 			
 			Iterator<Row> rows = sheet.rowIterator();
 			int count = 0;
 			while(rows.hasNext()){
-				count++;
-				if(count == 1){// 從第二行開始
-					continue;
-				}
 				Row row = rows.next();
+				count++;
+				if(count == 1){// 從第二列開始
+					continue;
+				}
 				Cell firstCell = row.getCell(0);
-				if(firstCell == null){
+
+				if(!validateCustomerCell(firstCell, count, warnings)){
 					continue;
 				}
-				int cellType = firstCell.getCellType();
-				if(cellType != Cell.CELL_TYPE_STRING){
+				
+				String customerName = firstCell.getStringCellValue().trim();
+				
+				if(!validateCustomer(customerName, count, warnings)){
 					continue;
 				}
-				String customerName = firstCell.getStringCellValue();
-				if(customerName == null || "".equals(customerName.trim())){
-					continue;
-				}
-				customerName = customerName.trim();
+				
 				List<String> details = shippingDetails.get(customerName);
 				if(details == null){
 					details = new ArrayList<>();
@@ -75,12 +153,18 @@ public class ShippingDetailsProcessor {
 				}
 				Cell secondCell = row.getCell(1);
 				if(secondCell.getCellType() != Cell.CELL_TYPE_STRING){
+					warnings.add("第" + count + "列產品/型號不是文字");
 					continue;
 				}
 				String productName = secondCell.getStringCellValue();
 				details.add(productName);
 			}
 		}
+				
+		if(!warnings.isEmpty()){
+			return outputData;
+		}
+		
 		LocalDateTime time = LocalDateTime.now();
 		int year = time.getYear();
 		int month = time.getMonth().getValue();
@@ -175,13 +259,23 @@ public class ShippingDetailsProcessor {
 				sheet.setSelected(true); // when selected, which enables Desktop.print multiple sheets
 				workbook.setPrintArea(sheetIdx, 0, lastCol, 0, currentRowIdx);
 			}
+//			for(int i = 0; i <= sheetIdx; i++){
+//				String printArea = workbook.getPrintArea(i);
+//				System.out.println(printArea);
+//			}
 			workbook.write(baos);
 			outputData = baos.toByteArray();
 		}
 		return outputData;
 	}
-
-	public static byte[] renderAgeteXlsx(byte[] data, Map<String, Object> options) throws Exception{
+	/**
+	 * 依據第一行的標題列決定欄位數
+	 * @param data
+	 * @param options
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] renderAgeteXlsx(byte[] data, Map<String, Object> options, List<String> warnings) throws Exception{
 		
 		Map<String, List<List<String>>> shippingDetails = new LinkedHashMap<>();
 		byte[] outputData = null;
@@ -192,12 +286,13 @@ public class ShippingDetailsProcessor {
 			Sheet sheet = workbook.getSheet("內容");
 			
 			if(sheet == null){
+				warnings.add("名稱為「內容」的sheet沒找到");
 				return outputData;
 			}
 			
 			Iterator<Row> rows = sheet.rowIterator();
 			int count = 0;
-			
+			int leastColumnCount = 2; // 依照常識判斷，至少兩行要有資料；其他沒有資料的欄位，只能當做是最後一行略過
 			while(rows.hasNext()){
 				count++;
 				if(count == 1){// 取得各欄位title
@@ -205,14 +300,22 @@ public class ShippingDetailsProcessor {
 					Iterator<Cell> firstRowCells = firstRow.cellIterator();
 					int columnCount = 0;
 					while(firstRowCells.hasNext()){
-						++columnCount;
 						Cell cell = firstRowCells.next();
-						if(cell.getCellType() != Cell.CELL_TYPE_STRING){
-							break;
+						++columnCount;
+						
+						if(cell.getCellType() != Cell.CELL_TYPE_STRING){// 除了第一行之外其他都要檢核是否為文字，因為標題列的資料需要在其他地方使用
+							if(columnCount == 1){
+								// 略過不檢查
+							}else if(columnCount <= leastColumnCount){
+								warnings.add("標題列第"+columnCount+"行不是文字");
+								continue;
+							}else{
+								break;
+							}
 						}
-						String title = cell.getStringCellValue();
-						if(columnCount != 1){
-							titles.add(title);
+						
+						if(columnCount != 1 && cell.getCellType() == Cell.CELL_TYPE_STRING){
+							titles.add(cell.getStringCellValue());
 						}
 						int columnWidth = sheet.getColumnWidth(columnCount-1);
 						columnWidths.add(columnWidth);
@@ -222,18 +325,16 @@ public class ShippingDetailsProcessor {
 								
 				Row row = rows.next();
 				Cell firstCell = row.getCell(0);
-				if(firstCell == null){
+				
+				if(!validateCustomerCell(firstCell, count, warnings)){
 					continue;
 				}
-				int cellType = firstCell.getCellType();
-				if(cellType != Cell.CELL_TYPE_STRING){
+				
+				String customerName = firstCell.getStringCellValue().trim();
+				
+				if(!validateCustomer(customerName, count, warnings)){
 					continue;
 				}
-				String customerName = firstCell.getStringCellValue();
-				if(customerName == null || "".equals(customerName.trim())){
-					continue;
-				}
-				customerName = customerName.trim();
 				
 				List<List<String>> details = shippingDetails.get(customerName);
 				if(details == null){
@@ -249,12 +350,21 @@ public class ShippingDetailsProcessor {
 					if(cell.getCellType() == Cell.CELL_TYPE_STRING){
 						val = cell.getStringCellValue();
 					}else if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC){
-						double num = cell.getNumericCellValue();
-						val = new BigDecimal(num).toPlainString();
+						if(!HSSFDateUtil.isCellDateFormatted(cell)){
+							double num = cell.getNumericCellValue();
+							val = new BigDecimal(num).toPlainString();
+						}else{
+							val = cell.getDateCellValue().toInstant().atZone(zoneId).toLocalDate().toString();
+							//val = LocalDate.from(cell.getDateCellValue().toInstant()).toString(); // 這段會出現錯誤，可能是沒有提供zoneId??
+						}
 					}
 					detail.add(val);
 				}
 			}
+		}
+		
+		if(!warnings.isEmpty()){
+			return outputData;
 		}
 		
 		LocalDateTime time = LocalDateTime.now();
