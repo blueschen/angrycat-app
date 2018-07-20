becab5f16a17d93bb9ea8ea4533fb0448289f423
yuanchi <eeit54@gmail.com>
2018-07-20 17:19:14 +0800

    updated

--- AngryCatERP/code/com/angrycat/erp/common/EmailContact.java
+++ AngryCatERP/code/com/angrycat/erp/common/EmailContact.java
@@ -7,9 +7,9 @@ package com.angrycat.erp.common;
 public class EmailContact {
 	public static final String IFLY = "iflywang@ohmbeads.com.tw";
 	public static final String BLUES = "blueschen@ohmbeads.com.tw";
-	public static final String MIKO = "mikofeng@ohmbeads.com.tw";
+	public static final String MIKO = "joannelin@ohmbeads.com.tw";
 	public static final String JOYCE = "joycechang@ohmbeads.com.tw";
 	public static final String JERRY = "jerrylin@ohmbeads.com.tw";
-	public static final String SLOW = "slowlywu@ohmbeads.com.tw";
+//	public static final String SLOW = "slowlywu@ohmbeads.com.tw";
 	public static final String INFO = "info@ohmbeads.com.tw";
 }
