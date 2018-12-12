59b10002d93b9f979924ac93d83a215e8cbc5161
yuanchi <eeit54@gmail.com>
2018-12-12 17:11:01 +0800

    EmailContact.java modified

--- AngryCatERP/code/com/angrycat/erp/common/EmailContact.java
+++ AngryCatERP/code/com/angrycat/erp/common/EmailContact.java
@@ -5,11 +5,12 @@ package com.angrycat.erp.common;
  *
  */
 public class EmailContact {
-	public static final String IFLY = "iflywang@ohmbeads.com.tw";
-	public static final String BLUES = "blueschen@ohmbeads.com.tw";
-	public static final String MIKO = "joannelin@ohmbeads.com.tw";
-	public static final String JOYCE = "joycechang@ohmbeads.com.tw";
-	public static final String JERRY = "jerrylin@ohmbeads.com.tw";
-//	public static final String SLOW = "slowlywu@ohmbeads.com.tw";
-	public static final String INFO = "info@ohmbeads.com.tw";
+	public static final String AT_OHM = "@ohmbeads.com.tw";
+	public static final String IFLY = "iflywang" + AT_OHM;
+	public static final String BLUES = "blueschen" + AT_OHM;
+	public static final String MIKO = "joannelin" + AT_OHM;
+//	public static final String JOYCE = "joycechang" + AT_OHM;
+	public static final String JERRY = "jerrylin" + AT_OHM;
+//	public static final String SLOW = "slowlywu@ohmbeads.com.tw" + AT_OHM;
+	public static final String INFO = "info" + AT_OHM;
 }
