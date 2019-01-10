37c9915d98239ae46acd3ace6cd2ca8e6ffcb918
yuanchi <eeit54@gmail.com>
2019-01-10 16:53:45 +0800

    ProductStockScheduleTask.java modified

--- AngryCatERP/code/com/angrycat/erp/scheduletask/ProductStockScheduleTask.java
+++ AngryCatERP/code/com/angrycat/erp/scheduletask/ProductStockScheduleTask.java
@@ -1,11 +1,5 @@
 package com.angrycat.erp.scheduletask;
 
-import static com.angrycat.erp.common.EmailContact.BLUES;
-import static com.angrycat.erp.common.EmailContact.IFLY;
-import static com.angrycat.erp.common.EmailContact.JERRY;
-import static com.angrycat.erp.common.EmailContact.MIKO;
-import static com.angrycat.erp.common.EmailContact.SLOW;
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +10,30 @@ import com.angrycat.erp.service.MailService;
 import com.angrycat.erp.service.ProductKendoUiService;
 import com.angrycat.erp.service.ProductKendoUiService.ProductStockReport;
 
+import static com.angrycat.erp.common.EmailContact.*;
+
 @Component
 public class ProductStockScheduleTask {
-//	@Autowired
-//	private ProductKendoUiService productKendoUiService;
-//	@Autowired
-//	private MailService mailService;
-//	
-//	@PostConstruct
-//	public void init(){
-//		mailService
-////			.to(MIKO, SLOW) 
-////			.cc(IFLY, JERRY, BLUES) // TODO 待上線後再啟用
-//			.subject("庫存狀態通知");
-//	}
-	// TODO 待上線後再啟用
-	/*
+	@Autowired
+	private ProductKendoUiService productKendoUiService;
+	@Autowired
+	private MailService mailService;
+	
+	@PostConstruct
+	public void init(){
+		mailService
+			.to(MIKO) 
+			.cc(IFLY, JERRY, BLUES)
+			.subject("庫存狀態通知");
+	}
 	@Scheduled(cron="0 55 23 * * ?")
-	public void updateStockIfMagentoIsMore(){
-		
-//		productKendoUiService.updateStockIfMagentoIsMore();
+	public void updateStockIfMagentoIsMore(){		
+		productKendoUiService.updateStockIfMagentoIsMore();
 	}
 	@Scheduled(cron="0 50 23 * * 1")
 	public void generateStockReport(){		
 		ProductStockReport report = productKendoUiService.generateStockReport();
 		mailService.content(report.toHtml())
 			.sendHTML();
-	}*/
+	}
 }
